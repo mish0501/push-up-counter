efdd86fdbf5f65deffb3723596d7732545495e7b
Mihail Georgiev <misho_0501@abv.bg>
2020-04-18 13:29:54 +0300

    Setting up gh-pages deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+git checkout --orphan gh-pages
+
+npm run build
+
+git --work-tree dist add --all
+git --work-tree dist commit -m 'gh-pages deploy'
+
+git push origin HEAD:gh-pages --force
+
+rm -r dist
+
+git checkout -f master
+
+git branch -D gh-pages
