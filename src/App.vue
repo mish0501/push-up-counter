7f0777d31d60b110b7a928ee9eabb3bce8b62555
Mihail Georgiev <misho_0501@abv.bg>
2020-04-18 17:54:02 +0300

    Added update button for new versions

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,17 @@
       <v-container fluid>
         <router-view />
       </v-container>
+
+      <v-snackbar v-model="snackWithButtons" :timeout="timeout" bottom>
+        {{ snackWithBtnText }}
+        <v-spacer />
+        <v-btn text color="lime " @click.stop="refreshApp">
+          {{ snackBtnText }}
+        </v-btn>
+        <v-btn dark icon @click="snackWithButtons = false">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-snackbar>
     </v-content>
   </v-app>
 </template>
@@ -40,6 +51,44 @@ export default {
 
   data: () => ({
     drawer: false,
+
+    refreshing: false,
+    registration: null,
+    snackBtnText: '',
+    snackWithBtnText: '',
+    snackWithButtons: false,
+    timeout: 0,
   }),
+
+  created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true })
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) return
+      this.refreshing = true
+      window.location.reload()
+    })
+  },
+  methods: {
+    showRefreshUI(e) {
+      // Display a snackbar inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      this.registration = e.detail
+      this.snackBtnText = 'Refresh'
+      this.snackWithBtnText = 'New version available!'
+      this.snackWithButtons = true
+    },
+    refreshApp() {
+      this.snackWithButtons = false
+      // Protect against missing registration.waiting.
+      if (!this.registration || !this.registration.waiting) {
+        return
+      }
+      this.registration.waiting.postMessage('skipWaiting')
+    },
+  },
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
   devServer: {
     https: true,
   },
+
   transpileDependencies: ['vuetify'],
   publicPath: process.env.NODE_ENV === 'production' ? '/push-up-counter/' : '/',
+
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    },
+  },
 }
